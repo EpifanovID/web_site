9e6bdcb64505f2d5913ca8e048060e9565e6bc7c
EpifanovIlia <idepifanov@edu.hse.ru>
2025-03-05 17:54:07 +0300

    Added inheritance and an about us page

--- myapp/urls.py
+++ myapp/urls.py
@@ -3,6 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('section1/', views.section1, name='section1'),
-    path('section2/', views.section2, name='section2'),
+    path('about/', views.about, name='about'),
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -3,8 +3,8 @@ from django.shortcuts import render
 def home(request):
     return render(request, 'home.html')
 
-def section1(request):
-    return render(request, 'section1.html')
+def about(request):
+    return render(request, 'about.html')
 
 def section2(request):
     return render(request, 'section2.html')
