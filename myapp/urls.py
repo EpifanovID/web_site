481b7306b848779ef6b9aee8e9598f52bf6fc250
EpifanovIlia <idepifanov@edu.hse.ru>
2025-03-06 15:18:17 +0300

    Added page with catalog and product

--- myapp/urls.py
+++ myapp/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
+    path('catalog/<str:name>/', views.product, name='product'),
+    path('catalog/', views.catalog, name='catalog'),
 ]
